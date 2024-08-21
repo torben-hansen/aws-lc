f55c2fdb67905f8389cb15bf42ddbcbf4c5b25b8
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2024-08-20 17:35:01 -0700

    Saving some description work

--- crypto/ube/ube.c
+++ crypto/ube/ube.c
@@ -35,56 +35,68 @@ static uint64_t get_fork_generation_number_mocked(void) {
   return 1;
 }
 
-// We define volatile
-// memory as memory containing data that must be unique for each usage to
-// maintain it's security properties. 
+// What is an "UBE"?
+// AWS-LC manages state that must be unique on each usage. For example, the
+// CTR-DRBG state must be unique on each usage, otherwise the generation
+// function will produce duplicated values. We name such state "volatile state"
+// or "volatile memory" if referring to memory. Before using a volatile state,
+// we must make sure to randomize it to preserve its uniqueness.
+//
+// When/how to randomize state depends on the context. For example, the CTR-DRBG
+// state, that is managed by AWS-LC, will randomize itself before usage during
+// normal operation. But there are events where this assumption is violated. We
+// call such events "uniqueness breaking events" (UBE). Forking an address space
+// is an example of an UBE.
+//
+// By detecting UBE's we can ensure that volatile state is properly randomized.
+// AWS-LC is currently able to detect two different type of UBE's that would
+// violate usage requirements of a volatile state. The two UBE types are:
+// Forking and VM resuming. Note, however, that detection methods rely on
+// technology that is unique to a platform. Hence, support for UBE detection
+// also depends on the platform AWS-LC is executed on.
+//
+// This file implements and manages the general machinery to detect an UBE. This
+// should be used by the rest of the code-base to implement proper randomization
+// of volatile states before usage.
 
 static CRYPTO_once_t ube_state_initialize_once = CRYPTO_ONCE_INIT;
 static CRYPTO_once_t ube_methods_unavailable_once = CRYPTO_ONCE_INIT; 
 static struct CRYPTO_STATIC_MUTEX ube_lock = CRYPTO_STATIC_MUTEX_INIT;
 static uint8_t ube_methods_unavailable = 0;
 
+// The UBE generation number is shared for the entire address space. One could
+// implement a per-thread UBE generation number. However, this could be
+// inefficient for global volatile states if care is not taken. Because every
+// thread only have visibility on their own per-thread generation number, we
+// could trigger a new randomization of the volatile state per-thread. While a
+// single randomization of the global volatile state should be sufficient.
+struct ube_global_state {
+  uint64_t generation_number;
+  uint64_t cached_fork_gn;
+  uint64_t cached_snapsafe_gn;
+};
+static struct ube_global_state ube_state = { 0 };
+
+// Convenience object that makes it easier to extend the number of detection
+// methods without having to modify function signatures.
 struct detection_gn {
 #define NUMBER_OF_DETECTION_GENERATION_NUMBERS 2
   uint64_t current_fork_gn;
   uint64_t current_snapsafe_gn;
 };
 
-struct ube_global_state {
-  uint64_t generation_number;
-  uint64_t cached_fork_gn;
-  uint64_t cached_snapsafe_gn;
-};
-
-static struct ube_global_state ube_state = { 0 };
-
-// Single mutation point of |ube_methods_unavailable|. Sets the variable to
-// 1 (true).
+// set_ube_methods_unavailable_once is the single mutation point of
+// |ube_methods_unavailable|. Sets the variable to 1 (true).
 static void set_ube_methods_unavailable_once(void) {
   ube_methods_unavailable = 1;
 }
 
-// ube_failed handles the failure path from 
+// ube_failed is a convenience function to synchronize mutation of
+// |ube_methods_unavailable|. In practice, synchronization is not strictly
+// needed because currently the mutation is only ever assigning 1 to the
+// variable.
 static void ube_failed(void) {
   CRYPTO_once(&ube_methods_unavailable_once, set_ube_methods_unavailable_once);
-}
-
-static int ube_get_detection_generation_numbers(
-  struct detection_gn *current_detection_gn) {
-
-  OPENSSL_STATIC_ASSERT(NUMBER_OF_DETECTION_GENERATION_NUMBERS == 2,
-    not_handling_the_exact_number_of_detection_generation_numbers);
-
-  current_detection_gn->current_fork_gn = get_fork_generation_number_mocked();
-  int ret_snapsafe_gn = get_snapsafe_generation_number_mocked(
-                          &(current_detection_gn->current_snapsafe_gn));
-
-  if (current_detection_gn->current_fork_gn == 0 ||
-      ret_snapsafe_gn == 0) {
-    return 0;
-  }
-
-  return 1;
 }
 
 static void ube_state_initialize(void) {
@@ -100,15 +112,9 @@ static void ube_state_initialize(void) {
   }
 }
 
-static int ube_is_detected(struct detection_gn *current_detection_gn) {
-
-  if (ube_state.cached_fork_gn != current_detection_gn->current_fork_gn ||
-      ube_state.cached_snapsafe_gn != current_detection_gn->current_snapsafe_gn) {
-    return 1;
-  }
-  return 0;
-}
-
+// ube_update_state updates the global state |ube_state| with the generation
+// numbers loaded into |current_detection_gn| and increments the UBE generation
+// number.
 static void ube_update_state(struct detection_gn *current_detection_gn) {
 
   ube_state.generation_number += 1;
@@ -120,6 +126,45 @@ static void ube_update_state(struct detection_gn *current_detection_gn) {
   ube_state.cached_snapsafe_gn = current_detection_gn->current_snapsafe_gn;
 }
 
+// ube_get_detection_generation_numbers loads the current detection generation
+// numbers into |current_detection_gn|.
+// Return 1 on success and 0 otherwise. The 0 return value means that a
+// detection method we expected to be available, is in fact not.
+static int ube_get_detection_generation_numbers(
+  struct detection_gn *current_detection_gn) {
+
+  // An attempt to prevent a situation where new detection methods are added but
+  // we forget to load them.
+  OPENSSL_STATIC_ASSERT(NUMBER_OF_DETECTION_GENERATION_NUMBERS == 2,
+    not_handling_the_exact_number_of_detection_generation_numbers);
+
+  current_detection_gn->current_fork_gn = get_fork_generation_number_mocked();
+  int ret_snapsafe_gn = get_snapsafe_generation_number_mocked(
+                          &(current_detection_gn->current_snapsafe_gn));
+
+  if (current_detection_gn->current_fork_gn == 0 ||
+      ret_snapsafe_gn == 0) {
+    return 0;
+  }
+
+  return 1;
+}
+
+// ube_is_detected computes whether a UBE has been detected or not by comparing
+// the cached detection generation numbers with the current detection generation
+// numbers. The current generation numbers must be loaded into
+// |current_detection_gn| before calling this function.
+// Return 1 if UBE has been detected and 0 if no UBE has been detected.
+static int ube_is_detected(struct detection_gn *current_detection_gn) {
+
+  if (ube_state.cached_fork_gn != current_detection_gn->current_fork_gn ||
+      ube_state.cached_snapsafe_gn != current_detection_gn->current_snapsafe_gn) {
+    return 1;
+  }
+  return 0;
+}
+
+// stays invariant after a failure state has been reached.
 int get_ube_generation_number(uint64_t *current_generation_number) {
 
   GUARD_PTR(current_generation_number);
@@ -128,16 +173,18 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
   CRYPTO_once(&ube_state_initialize_once, ube_state_initialize);
 
   // If something failed at an earlier point short-circuit immediately. This
-  // check must be done after attempting to initialize the UBE state. 
+  // check must be done after attempting to initialize the UBE state. Because
+  // initialization might fail and we can short-circuit here.
   if (ube_methods_unavailable == 1) {
     return 0;
   }
 
   struct detection_gn current_detection_gn = { 0 };
 
-  // First read generation numbers for each detection method supported. We do
+  // First read generation numbers for each supported detection method. We do
   // not mutate |ube_state|. So, a read lock is sufficient at this point. Each
-  // individual detection method will have their own concurrency controls.
+  // individual detection method will have their own concurrency controls if
+  // needed.
 
   CRYPTO_STATIC_MUTEX_lock_read(&ube_lock);
   if (ube_get_detection_generation_numbers(&current_detection_gn) != 1) {
@@ -146,7 +193,7 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
     return 0;
   }
   if (ube_is_detected(&current_detection_gn) == 0) {
-    // No UBE detected, so just grab generation number from state.
+    // No UBE detected, so just grab UBE generation number from the state.
     *current_generation_number = ube_state.generation_number;
     CRYPTO_STATIC_MUTEX_unlock_read(&ube_lock);
     return 1;
@@ -154,13 +201,13 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
   CRYPTO_STATIC_MUTEX_unlock_read(&ube_lock);
 
   // Reaching this point means that an UBE has been detected. We must now
-  // synchronize an update to ube_state.generation_number. To avoid redundant
-  // reseed, we must ensure the generation number is only incremented once for
-  // the all UBE's that might have happened. Therefore, first take a write lock
-  // but before writing mutation the state, check for an UBE again.
+  // synchronize an update to the UBE generation number. To avoid redundant
+  // reseeds, we must ensure the generation number is only incremented once for
+  // all UBE's that might have happened. Therefore, first take a write lock but
+  // before mutation the state, check for an UBE again.
 
   CRYPTO_STATIC_MUTEX_lock_write(&ube_lock);
-  // Method generation numbers might have changed. Grab each one again.
+  // Detection generation numbers might have changed. Load all again.
   if (ube_get_detection_generation_numbers(&current_detection_gn) != 1) {
     ube_failed();
     CRYPTO_STATIC_MUTEX_unlock_write(&ube_lock);
@@ -168,7 +215,8 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
   }
 
   if (ube_is_detected(&current_detection_gn) == 0) {
-    // No UBE detected, so just grab generation number from state.
+    // Another thread already update the global state. Just load the UBE
+    // generation number from there.
     *current_generation_number = ube_state.generation_number;
     CRYPTO_STATIC_MUTEX_unlock_write(&ube_lock);
     return 1;
