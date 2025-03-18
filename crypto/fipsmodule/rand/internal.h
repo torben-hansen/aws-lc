ae498207abfd22633c5c5d0062868861b8365521
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2025-03-18 14:56:46 -0700

    Oops...

--- crypto/fipsmodule/rand/internal.h
+++ crypto/fipsmodule/rand/internal.h
@@ -28,7 +28,7 @@
 extern "C" {
 #endif
 
-#define PRINT_ABORT_DEBUG fprintf(stderr, "%s(): calling abort()\n", __func__); fflush();
+#define PRINT_ABORT_DEBUG fprintf(stderr, "%s(): calling abort()\n", __func__); fflush(stderr);
 
 #if defined(BORINGSSL_UNSAFE_DETERMINISTIC_MODE)
 #define OPENSSL_RAND_DETERMINISTIC
